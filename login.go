c271801fed11e149b6e7c0640c6f22f9849ca0cd
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-04-14 16:52:02 -0700

    Connection message

--- login.go
+++ login.go
@@ -147,6 +147,7 @@ func GuildLogin(name string) {
 	_, rsp := Send(MethodGuild, map[string]any{"gld": name})
 	if rsp.Error == nil {
 		guild = name
+		Write("gld", "Successfully connected to guild %s!", name)
 	} else {
 		Error("gld", "%s", *rsp.Error)
 	}
